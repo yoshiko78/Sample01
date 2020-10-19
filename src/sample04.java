6fe66622e32d19e7be15d610d197134106e51c62
Maro2 <Maro2@192.168.0.21>
2020-10-19 11:09:44 +0900

    aaaaaaaa

---
+++ src/sample04.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Maro2
+ *
+ */
+public class sample04 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("ohayougozaimasu");
+
+	}
+
+}
